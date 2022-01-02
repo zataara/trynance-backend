77a6e24c66b102df17e4482c8f708052b94aca4f
zataara <bryantmac@gmail.com>
2022-01-02 11:09:59 -0800

    added missing parens in query string

--- tests/_testTemplates.js
+++ tests/_testTemplates.js
@@ -24,7 +24,7 @@ async function _beforeAll() {
                                 last_name,
                                 email)
               VALUES ('user1', $1, 'John', 'Doe', 'john@doe.com'),
-                      'user2', $2, 'Jane', 'Doe', 'jane@doe.com')
+                      ('user2', $2, 'Jane', 'Doe', 'jane@doe.com')
               RETURNING username`,
               [await bcrypt.hash("john1234", BCRYPT_WORK_FACTOR),
               await bcrypt.hash("jane1234", BCRYPT_WORK_FACTOR)]);

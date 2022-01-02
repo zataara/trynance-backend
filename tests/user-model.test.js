eeacc0d4bfe3e506392d65d03d8e184263df8598
zataara <bryantmac@gmail.com>
2022-01-02 11:10:42 -0800

    added basic user registration tests

--- tests/user-model.test.js
+++ tests/user-model.test.js
@@ -2,10 +2,53 @@
 
 const { NotFoundError, BadRequestError, UnauthorizedError, } = require("../expressError");
 const db = require("../db.js");
-const User = require("./user.js");
-const { commonBeforeAll, commonBeforeEach, commonAfterEach, commonAfterAll } = require("./_testCommon");
+const User = require("../models/user");
+const { _beforeEach, _afterEach, _beforeAll, _afterAll} = require("./_testTemplates");
 
-beforeAll(commonBeforeAll);
-beforeEach(commonBeforeEach);
-afterEach(commonAfterEach);
-afterAll(commonAfterAll);
+beforeAll(_beforeAll);
+beforeEach(_beforeEach);
+afterEach(_afterEach);
+afterAll(_afterAll);
+
+
+/*** Tesing User Registration ***/
+
+describe("Registering a new user", () => {
+  const newUser = {
+    username: "test-user1",
+    firstName: "John",
+    lastName: "Smith",
+    email: "johnsmith@gmail.com",
+  };
+
+  test("registration", async () => {
+    let currentUser = await User.register({
+      ...newUser,
+      password: "john1234",
+    });
+    expect(currentUser).toEqual(newUser);
+
+    const findUser = await db.query(`SELECT *
+                                    FROM users
+                                    WHERE username = 'test-user1'`);
+    expect(findUser.rows.length).toEqual(1);
+    expect(findUser.rows[0].password.startsWith('$2b$')).toEqual(true);
+  });
+
+  test("get a BadRequestError with duplicate data", async () => {
+    try{
+      await User.register({
+        ...newUser,
+        password: "john1234",
+      });
+
+      await User.register({
+        ...newUser,
+        password: "john1234",
+      });
+      fail();
+    } catch(e) {
+      expect(e instanceof BadRequestError).toBeTruthy();
+    }
+  });
+});

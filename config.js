580665bb16a8e9abd04950be8de8b8e92b2a5c1a
zataara <bryantmac@gmail.com>
2022-01-01 22:38:50 -0800

    added initial app configurations

---
+++ config.js
@@ -0,0 +1,33 @@
+"use strict";
+
+require("dotenv").config();
+require("colors");
+
+const SECRET_KEY = process.env.SECRET_KEY || "secret-key";
+
+const PORT = +process.env.PORT || 3003;
+
+function getDatabaseUri() {
+  return process.env.NODE_ENv === "test"
+    ? "trynance_test"
+    : process.env.DATABASE_URL || "trynance";
+}
+
+//Lower bcrypt's work factor during test and raise during production.
+const BCRYPT_WORK_FACTOR = process.env.NODE_ENV === "test" ? 1 : 12;
+
+console.log("------------------------------");
+console.log("Tryance Config:".green);
+console.log("SECRET_KEY:".yellow, SECRET_KEY);
+console.log("PORT:".yellow, PORT.toString());
+console.log("BCRYPT_WORK_FACTOR".yellow, BCRYPT_WORK_FACTOR);
+console.log("Database:".blue, getDatabaseUri());
+console.log("------------------------------")
+
+
+module.exports = {
+  SECRET_KEY,
+  PORT,
+  BCRYPT_WORK_FACTOR,
+  getDatabaseUri,
+}

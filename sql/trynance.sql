89181a987af826c215c8e1847f1e80a512203740
zataara <bryantmac@gmail.com>
2022-01-01 23:45:42 -0800

    added sql script for quick db washing

---
+++ sql/trynance.sql
@@ -0,0 +1,18 @@
+\echo 'Delete and recreate trynance db?'
+\prompt 'Return for yes or control-C to cancel > ' foo
+
+DROP DATABASE trynance;
+CREATE DATABASE trynance;
+\connect trynance;
+
+\i tryanance-schema.sql
+\i trynance-seed.sql
+
+\echo 'Delete and recreate trynance_test db?'
+\prompt 'Return for yes or control-C to cancel > ' foo
+
+DROP DATABASE trynance_test;
+CREATE DATABASE trynance_test;
+\connect trynance_test
+
+\i trynance-schema.sql

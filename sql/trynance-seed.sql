b2daa19b3aa0496dc7e707fec638c0c3a80663f0
zataara <bryantmac@gmail.com>
2022-01-01 23:46:00 -0800

    added test user to seed file

---
+++ sql/trynance-seed.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (username, password, first_name, last_name, email)
+VALUES ('testuser',
+        'password',
+        'TestFirstName',
+        'TestLastName',
+        'test@test.com')

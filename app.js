7533102c700a9adac7b743bb090437ee8f68bac1
zataara <bryantmac@gmail.com>
2022-01-02 17:20:18 -0800

    added morgan and added error stack logging

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(morgan("tiny"));
 app.use(authenticateJWT);
 
 app.use("/auth", authRoutes);
@@ -27,8 +28,10 @@ app.use(function (err, req, res, next) {
   if (process.env.NODE_ENV !== "test") {
     console.error(err.stack);
   }
+  
   const status = err.status || 500;
   const message = err.message;
+  console.log(err.stack)
 
   return res.status(status).json({
     error: { message, status }

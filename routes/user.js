29ef9c73fc18a5a4d8850e27f063b07d2fb7e6aa
zataara <bryantmac@gmail.com>
2022-01-11 18:44:22 -0800

    added new routes for faves, trades, and assets

--- routes/user.js
+++ routes/user.js
@@ -4,6 +4,10 @@ const express = require("express");
 const { ensureCorrectUserOrAdmin } = require("../middleware/auth");
 const { BadRequestError } = require("../expressError");
 const User = require("../models/user");
+const Trades = require("../models/trades");
+const Assets = require("../models/assets");
+const Faves = require("../models/faves");
+
 const { createToken } = require("../helpers/tokens");
 const jsonschema = require("jsonschema");
 const userNewSchema = require("../schemas/userNew.json");
@@ -17,6 +21,73 @@ router.get(
     try {
       const user = await User.get(req.params.username);
       return res.json({ user });
+    } catch (e) {
+      return next(e);
+    }
+  }
+);
+
+router.get(
+  "/:username/assets",
+  ensureCorrectUserOrAdmin,
+  async function (req, res, next) {
+    try {
+      return Assets.getAll(req.params.username);
+    } catch (e) {
+      return next(e);
+    }
+  }
+);
+
+router.get(
+  "/:username/trades",
+  ensureCorrectUserOrAdmin,
+  async function (req, res, next) {
+    try {
+      return Trades.getAll(req.params.username);
+    } catch (e) {
+      return next(e);
+    }
+  }
+);
+
+router.post(
+  "/:username/trades/:cfa/:cf/:cta/:ct/:dt",
+  ensureCorrectUserOrAdmin,
+  async function (req, res, next) {
+    try {
+      return Trades.postTrade(
+        req.params.username,
+        req.params.cfa,
+        ret.params.cd,
+        req.params.cta,
+        req.params.ct,
+        req.params.dt
+      );
+    } catch (e) {
+      return next(e);
+    }
+  }
+);
+
+router.get(
+  "/:username/faves",
+  ensureCorrectUserOrAdmin,
+  async function (req, res, next) {
+    try {
+      return res.json(Faves.getAll(req.params.username));
+    } catch (e) {
+      return next(e);
+    }
+  }
+);
+
+router.post(
+  "/:username/faves/:fave",
+  ensureCorrectUserOrAdmin,
+  async function (req, res, next) {
+    try {
+      return Faves.postFave(req.params.username, req.params.fave);
     } catch (e) {
       return next(e);
     }

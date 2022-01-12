b5ffdfb53492f6bf622d09c76a59146ec8feda0f
zataara <bryantmac@gmail.com>
2022-01-11 18:43:18 -0800

    deleted unneeded statements

--- models/assets.js
+++ models/assets.js
@@ -7,24 +7,16 @@ const {
   UnauthorizedError,
 } = require("../expressError");
 
-
-
 class Assets {
-  
   static async getAll(username) {
-    try {
     const assets = await db.query(
       `SELECT * 
       FROM assets
       WHERE user_id = $1`,
       [username]
     );
-    return res.json({ assets });
-  } catch (e) {
-    return next(e);
-    }
+    return JSON.stringify( assets.rows );
   }
-  
 }
 
 module.exports = Assets;

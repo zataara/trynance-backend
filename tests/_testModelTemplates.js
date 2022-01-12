f184bc2f703c84484c55cf0184f62060a30a47be
zataara <bryantmac@gmail.com>
2022-01-11 18:45:02 -0800

    added new db seeding to _beforeAll funcs

--- tests/_testModelTemplates.js
+++ tests/_testModelTemplates.js
@@ -22,6 +22,48 @@ async function _BeforeAll() {
       await bcrypt.hash("password2", BCRYPT_WORK_FACTOR),
     ]
   );
+
+  await db.query("DELETE FROM assets");
+
+  await db.query(
+    `INSERT INTO assets(symbol,
+                          user_id,
+                          amount)
+        VALUES('btc', 'u1', 1),
+              ('eth', 'u1', 1),
+              ('xrp', 'u1', 200)               
+        `
+  );
+
+  await db.query("DELETE FROM trades");
+
+  await db.query(
+    `INSERT INTO trades(
+                          user_id,
+                          currency_from_amount,
+                          currency_from,
+                          currency_to_amount,
+                          currency_to,
+                          datetime)
+        VALUES('u1', 1, 'btc', 1, 'eth', 
+        'time'),
+              ('u1', 1, 'eth', 1, 'sol', 'time'),
+              ('u1', 200, 'xrp', 200, 'xlm', 'time')               
+        `
+  );
+
+  await db.query("DELETE FROM faves");
+
+  await db.query(
+    `INSERT INTO faves(
+                        symbol,
+                        user_id
+                          )
+        VALUES('btc', 'u1'),
+              ('eth', 'u1'),
+              ('xlm', 'u1')               
+        `
+  );
 }
 
 async function _BeforeEach() {

7c524ce1281826b1683c2afd287f0f381ac8697c
zataara <bryantmac@gmail.com>
2022-01-11 18:45:11 -0800

    changing login function

--- tests/auth-middleware.test.js
+++ tests/auth-middleware.test.js
@@ -4,7 +4,7 @@ const jwt = require("jsonwebtoken");
 const { UnauthorizedError } = require("../expressError");
 const {
   authenticateJWT,
-  ensureLoggedIn,
+  ensureCorrectUserOrAdmin,
   
 } = require("../middleware/auth");
 
@@ -58,25 +58,45 @@ describe("authenticateJWT", function () {
 });
 
 
-describe("ensureLoggedIn", function () {
-  test("works", function () {
+describe("ensureCorrectUserOrAdmin", function () {
+  test("works: admin", function () {
     expect.assertions(1);
-    const req = {};
-    const res = { locals: { user: { username: "test", is_admin: false } } };
+    const req = { params: { username: "test" } };
+    const res = { locals: { user: { username: "admin", isAdmin: true } } };
     const next = function (err) {
       expect(err).toBeFalsy();
     };
-    ensureLoggedIn(req, res, next);
+    ensureCorrectUserOrAdmin(req, res, next);
   });
 
-  test("unauth if no login", function () {
+  test("works: same user", function () {
     expect.assertions(1);
-    const req = {};
+    const req = { params: { username: "test" } };
+    const res = { locals: { user: { username: "test", isAdmin: false } } };
+    const next = function (err) {
+      expect(err).toBeFalsy();
+    };
+    ensureCorrectUserOrAdmin(req, res, next);
+  });
+
+  test("unauth: mismatch", function () {
+    expect.assertions(1);
+    const req = { params: { username: "wrong" } };
+    const res = { locals: { user: { username: "test", isAdmin: false } } };
+    const next = function (err) {
+      expect(err instanceof UnauthorizedError).toBeTruthy();
+    };
+    ensureCorrectUserOrAdmin(req, res, next);
+  });
+
+  test("unauth: if anon", function () {
+    expect.assertions(1);
+    const req = { params: { username: "test" } };
     const res = { locals: {} };
     const next = function (err) {
       expect(err instanceof UnauthorizedError).toBeTruthy();
     };
-    ensureLoggedIn(req, res, next);
+    ensureCorrectUserOrAdmin(req, res, next);
   });
 });
 

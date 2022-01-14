b0f620c0da62cfc6ecaf1dde27aef8179764a485
zataara <bryantmac@gmail.com>
2022-01-13 22:31:25 -0800

    restructured trade logic pipeline

--- models/trades.js
+++ models/trades.js
@@ -7,9 +7,8 @@ const {
   UnauthorizedError,
 } = require("../expressError");
 
-
 class Trades {
-  static async getAll(username) {
+  static async get(username) {
     const trades = await db.query(
       `SELECT * 
         FROM trades
@@ -19,22 +18,7 @@ class Trades {
     return JSON.stringify(trades.rows);
   }
   static async post(username, cfa, cf, cta, ct, dt) {
-    // Insert the new trade into the trades table
-    const newTrade = await db.query(
-      `INSERT INTO trades
-        (user_id,
-        currency_from_amount,
-        currency_from,
-        currency_to_amount,
-        currency_to,
-        datetime)
-        VALUES ($1, $2, $3, $4, $5, $6)
-        RETURNING user_id AS username, currency_from_amount AS "currencyFromAmount", currency_from AS "currencyFrom", currency_to_amount AS "currencyToAmount", currency_to AS "currencyTo", datetime`,
-      [username, cfa, cf, cta, ct, dt]
-    );
-    
-
-    // Select the amount of a certain asset a user has and update the new asset amount after trade
+    // First select the amount of a certain asset a user has and update the new asset amount after trade. If there is not enough of the currency_from_amount, trade will fail.
     const assetsFrom = await db.query(
       `SELECT * 
           FROM assets
@@ -42,51 +26,63 @@ class Trades {
           AND user_id = $2`,
       [cf, username]
     );
-    if(assetsFrom.rows[0] === undefined) {
-      return JSON.stringify({status: "failed: insufficient funds"})
-    }
-    if(assetsFrom.rows[0]['amount']){
-    let newAmount = assetsFrom.rows[0]['amount'] - cfa;
-
-    if(newAmount < 0) {
-      return JSON.stringify({status: "failed: insufficient funds"})
-    }
-    console.log(newAmount, cf, username);
-    let updated = await db.query(
-      `UPDATE assets
-          SET amount = $1
-          WHERE symbol = $2
-          AND user_id = $3
-          RETURNING symbol, user_id AS "username", amount`,
-      [newAmount, cf, username]
-    );
-    console.log(updated.rows[0]);
-    } else {
-      return JSON.stringify({status: "failed: insufficient funds"})
+    if (
+      assetsFrom.rows[0] === undefined ||
+      assetsFrom.rows[0]["amount"] - cfa < 0
+    ) {
+      return JSON.stringify({ status: "Failed: insufficient funds" });
     }
 
-    //check to see if the user already has some of the asset that they are exchanging to, if so update the new amount, if not create a new row with the new asset.
-    const assetsTo = await db.query(
-      `SELECT * 
-          FROM assets
-          WHERE symbol = $1
-          AND user_id = $2`,
-      [ct, username]
-    );
-    if (assetsTo.rows[0]) {
-      const currentAssetAmountTo = 
-        JSON.stringify(assetsTo.rows[0]["amount"]
+    //initiate making a trade
+    try {
+
+      const newAmount = assetsFrom.rows[0]["amount"] - cfa; 
+      const newTrade = await db.query(
+        `INSERT INTO trades
+          (user_id,
+          currency_from_amount,
+          currency_from,
+          currency_to_amount,
+          currency_to,
+          datetime)
+          VALUES ($1, $2, $3, $4, $5, $6)
+          RETURNING user_id AS username, currency_from_amount AS "currencyFromAmount", currency_from AS "currencyFrom", currency_to_amount AS "currencyToAmount", currency_to AS "currencyTo", datetime`,
+        [username, cfa, cf, cta, ct, dt]
       );
-      await db.query(
+      console.log("New Trade Inserted", newTrade.rows[0]);
+
+      let updatedAsset = await db.query(
         `UPDATE assets
             SET amount = $1
             WHERE symbol = $2
-            AND user_id = $3`,
-        [currentAssetAmountTo + cta, ct, username]
+            AND user_id = $3
+            RETURNING symbol, user_id AS "username", amount`,
+        [newAmount, cf, username]
       );
-    } else {
-      await db.query(
-        `INSERT INTO assets
+      console.log("Currency From Amount Updated:", updatedAsset.rows[0]);
+
+      //check to see if the user already has some of the asset that they are exchanging to, if so update the new amount, if not create a new row with the new asset.
+      const assetsTo = await db.query(
+        `SELECT * 
+          FROM assets
+          WHERE symbol = $1
+          AND user_id = $2`,
+        [ct, username]
+      );
+      if (assetsTo.rows[0]) {
+        const currentAssetAmountTo = JSON.stringify(assetsTo.rows[0]["amount"]);
+        const updatedAsset = await db.query(
+          `UPDATE assets
+            SET amount = $1
+            WHERE symbol = $2
+            AND user_id = $3
+            RETURNING symot, amount`,
+          [currentAssetAmountTo + cta, ct, username]
+        );
+        console.log("Currency To Amount Updated", updatedAsset.rows[0])
+      } else {
+        const newAsset = await db.query(
+          `INSERT INTO assets
             (
               symbol,
               user_id,
@@ -95,8 +91,14 @@ class Trades {
             VALUES ($1, $2, $3)
             RETURNING symbol, user_id AS "username", amount
             `,
-        [ct, username, cta]
-      );
+          [ct, username, cta]
+        );
+        console.log("Currency To Amount Created", newAsset.rows[0])
+      }
+    } catch (e) {
+      console.log({ Status: "Failed" });
+      console.log(e.stack)
+      return e.stack;
     }
   }
 }

af97f1472b44561336759253bcc581813ab1058b
zataara <bryantmac@gmail.com>
2022-01-17 22:09:12 -0800

    added new seed for trades

--- sql/trynance-seed.sql
+++ sql/trynance-seed.sql
@@ -27,6 +27,9 @@ VALUES ('dot', 'testuser', 4),
       ('busd', 'testuser', 5000),
       ('atom', 'testuser', 22);
 
-
+INSERT INTO trades (user_id, currency_from_amount, currency_from_price, currency_from, currency_to_amount, currency_to_price, currency_to, date)
+VALUES ('testuser', 1, 63000, 'btc', 10, 6000, 'eth', 'Mon Jan 1 2022' ), 
+      ('testuser', 10, 2, 'xrp', 10, 1, 'xlm', 'Mon Jan 8 2022'), 
+      ('testuser', 1, 1000000, 'shib', 1, 60, 'eos','Mon Jan 17 2022');
 
         

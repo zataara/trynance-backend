bb9b3c040d2456eb861609158e1603b02da79c31
zataara <bryantmac@gmail.com>
2022-01-19 02:05:37 -0800

    updated variables

--- models/trades.js
+++ models/trades.js
@@ -17,46 +17,76 @@ class Trades {
     );
     return JSON.stringify(trades.rows);
   }
-  static async post(username, cfa, cf, cta, ct, dt) {
+  static async post({ data }) {
+    let {
+      username,
+      currency_from_amount,
+      currency_from_price,
+      currency_from_image,
+      currency_from,
+      currency_to_amount,
+      currency_to_price,
+      currency_to_image,
+      currency_to,
+      date,
+    } = data;
+    console.log(data);
+
     // First select the amount of a certain asset a user has and update the new asset amount after trade. If there is not enough of the currency_from_amount, trade will fail.
     const assetsFrom = await db.query(
       `SELECT * 
           FROM assets
           WHERE symbol = $1
           AND user_id = $2`,
-      [cf, username]
+      [currency_from, username]
     );
+
     if (
       assetsFrom.rows[0] === undefined ||
-      assetsFrom.rows[0]["amount"] - cfa < 0
+      assetsFrom.rows[0]["amount"] - currency_from_amount < 0
     ) {
       return JSON.stringify({ status: "Failed: insufficient funds" });
     }
 
     //initiate making a trade
     try {
-      const newAmount = assetsFrom.rows[0]["amount"] - cfa;
       const newTrade = await db.query(
         `INSERT INTO trades
           (user_id,
           currency_from_amount,
+          currency_from_price,
+          currency_from_image,
           currency_from,
           currency_to_amount,
+          currency_to_price,
+          currency_to_image,
           currency_to,
-          datetime)
-          VALUES ($1, $2, $3, $4, $5, $6)
-          RETURNING user_id AS username, currency_from_amount AS "currencyFromAmount", currency_from AS "currencyFrom", currency_to_amount AS "currencyToAmount", currency_to AS "currencyTo", datetime`,
-        [username, cfa, cf, cta, ct, dt]
+          date)
+          VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
+          RETURNING user_id AS username, currency_from_amount AS "currencyFromAmount", currency_from_price AS "currencyFromPrice", currency_from_image AS "currencyFromImage", currency_from AS "currencyFrom", currency_to_amount AS "currencyToAmount", currency_to_price AS "currencyToPrice", currency_to_image AS "currencyToImage", currency_to AS "currencyTo", date`,
+        [
+          username,
+          currency_from_amount,
+          currency_from_price,
+          currency_from_image,
+          currency_from,
+          currency_to_amount,
+          currency_to_price,
+          currency_to_image,
+          currency_to,
+          date,
+        ]
       );
       console.log("New Trade Inserted", newTrade.rows[0]);
 
+      const newAmount = assetsFrom.rows[0]["amount"] - currency_from_amount;
       let updatedAsset = await db.query(
         `UPDATE assets
             SET amount = $1
             WHERE symbol = $2
             AND user_id = $3
             RETURNING symbol, user_id AS "username", amount`,
-        [newAmount, cf, username]
+        [newAmount, currency_from, username]
       );
       console.log("Currency From Amount Updated:", updatedAsset.rows[0]);
 
@@ -66,31 +96,34 @@ class Trades {
           FROM assets
           WHERE symbol = $1
           AND user_id = $2`,
-        [ct, username]
+        [currency_to, username]
       );
       if (assetsTo.rows[0]) {
-        const currentAssetAmountTo = JSON.stringify(assetsTo.rows[0]["amount"]);
+        const currentAssetAmountTo = assetsTo.rows[0]["amount"];
+        console.log(currentAssetAmountTo);
         const updatedAsset = await db.query(
           `UPDATE assets
             SET amount = $1
             WHERE symbol = $2
             AND user_id = $3
-            RETURNING symot, amount`,
-          [currentAssetAmountTo + cta, ct, username]
+            RETURNING symbol, amount`,
+          [currentAssetAmountTo + currency_to_amount, currency_to, username]
         );
         console.log("Currency To Amount Updated", updatedAsset.rows[0]);
       } else {
+        console.log("hereeeee")
         const newAsset = await db.query(
           `INSERT INTO assets
-            (
-              symbol,
+            (symbol,
               user_id,
-              amount
+              amount,
+              image,
+              name
             )
-            VALUES ($1, $2, $3)
+            VALUES ($1, $2, $3, $4, $5)
             RETURNING symbol, user_id AS "username", amount
             `,
-          [ct, username, cta]
+          [currency_to, username, currency_to_amount, currency_to_image, currency_to]
         );
         console.log("Currency To Amount Created", newAsset.rows[0]);
       }
